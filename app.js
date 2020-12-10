d4f1f17c094b1baf051d649e4c317bf00df81a6c
ararita <munjizavesna@gmail.com>
2020-12-10 11:58:42 +0100

    Add models

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
 mongoose
-  .connect('mongodb://localhost/projector', { useNewUrlParser: true })
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .connect("mongodb://localhost/projector", { useNewUrlParser: true })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
-const session = require('express-session');
-const passport = require('passport');
+const session = require("express-session");
+const passport = require("passport");
 
-require('./configs/passport.js');
+require("./configs/passport.js");
 
-const MongoStore = require('connect-mongo')(session);
-
+const MongoStore = require("connect-mongo")(session);
 
 const app = express();
 
@@ -40,48 +42,45 @@ app.use(
     resave: true,
     store: new MongoStore({
       // when the session cookie has an expiration date
-      // connect-mongo will use it, otherwise it will create a new 
+      // connect-mongo will use it, otherwise it will create a new
       // one and use ttl - time to live - in that case one day
       mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60 * 1000
-    })
+      ttl: 24 * 60 * 60 * 1000,
+    }),
   })
-)
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true,
+  })
+);
 
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
+const businesses = require("./routes/businesses");
+app.use("/api/businesses", businesses);
 
-
-const projects = require('./routes/projects');
-app.use('/api/projects', projects);
-
-const auth = require('./routes/auth');
-app.use('/api/auth', auth);
+const auth = require("./routes/auth");
+app.use("/api/auth", auth);
 
 module.exports = app;
