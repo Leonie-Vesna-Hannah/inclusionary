eb271f70974506e740fb1481da85944c0da71045
ararita <munjizavesna@gmail.com>
2020-12-10 14:44:08 +0100

    Add form

--- client/src/components/AddBusiness.js
+++ client/src/components/AddBusiness.js
@@ -6,6 +6,14 @@ export default class AddBusiness extends Component {
   state = {
     title: "",
     description: "",
+    picture: "",
+    category: [],
+    street: "",
+    houseNumber: null,
+    city: "",
+    zipCode: null,
+    email: "",
+    // design: [],
   };
 
   handleChange = (event) => {
@@ -24,12 +32,28 @@ export default class AddBusiness extends Component {
       .post("/api/businesses", {
         title: this.state.title,
         description: this.state.description,
+        picture: this.state.picture,
+        category: this.state.category,
+        street: this.state.street,
+        houseNumber: this.state.houseNumber,
+        city: this.state.city,
+        zipCode: this.state.zipCode,
+        email: this.state.email,
+        // design: this.state.design,
       })
       .then(() => {
         // set the form to it's initial state (empty input fields)
         this.setState({
           title: "",
           description: "",
+          picture: "",
+          category: [],
+          street: "",
+          houseNumber: null,
+          city: "",
+          zipCode: null,
+          email: "",
+          // design: [],
         });
         // update the parent components state (in Projects) by calling getData()
         this.props.getData();
@@ -56,6 +80,96 @@ export default class AddBusiness extends Component {
             id="description"
             name="description"
             value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="picture">Picture: </Form.Label>
+          <Form.Control
+            type="text"
+            id="picture"
+            name="picture"
+            value={this.state.picture}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="category">Category: </Form.Label>
+          <Form.Control
+            type="text"
+            id="category"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="street">Street: </Form.Label>
+          <Form.Control
+            type="text"
+            id="street"
+            name="street"
+            value={this.state.street}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="houseNumber">House number: </Form.Label>
+          <Form.Control
+            type="text"
+            id="houseNumber"
+            name="houseNumber"
+            value={this.state.houseNumber}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="city">City: </Form.Label>
+          <Form.Control
+            type="text"
+            id="city"
+            name="city"
+            value={this.state.city}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="zipCode">ZIP Code: </Form.Label>
+          <Form.Control
+            type="text"
+            id="zipCode"
+            name="zipCode"
+            value={this.state.zipCode}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="country">Country: </Form.Label>
+          <Form.Control
+            type="text"
+            id="country"
+            name="country"
+            value={this.state.country}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="email">E-mail: </Form.Label>
+          <Form.Control
+            type="text"
+            id="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="design">Design: </Form.Label>
+          <Form.Control
+            type="text"
+            id="design"
+            name="design"
+            value={this.state.design}
             onChange={this.handleChange}
           />
         </Form.Group>
